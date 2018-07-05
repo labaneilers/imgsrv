48f0afbf76879edb3dc7d547f20444f03a44db4c
Laban Eilers <leilers@vistaprint.com>
2018-07-05 13:30:07 -0400

    Enforce querystring order

--- api.js
+++ api.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const querystring = require('querystring');
+const url = require('url');
+
+function getCanonicalQueryString(options) {
+  let canonicalQs = {
+    u: options.uri,
+    w: options.width
+  };
+
+  if (options.allowWebp) {
+    canonicalQs.webp = "1";
+  } 
+  if (options.allowJp2) {
+    canonicalQs.jp2 = "1";
+  }
+
+  return querystring.stringify(canonicalQs);
+}
+
+function validateCanonicalQuerystring(request, options)
+{
+  let canonicalQs = getCanonicalQueryString(options);
+  let suppliedQs = url.parse(request.url).query;
+
+  if (canonicalQs != suppliedQs) {
+    throw new Error(`Params order and encoding is strict to enforce cachability\nCanonical: ${canonicalQs}\nActual:${suppliedQs}`);
+  }
+}
+
 function parseParams(request) {
 
     let options = {
@@ -8,9 +37,6 @@ function parseParams(request) {
         allowWebp: request.query.webp == "1",
         allowJp2: request.query.jp2 == "1"
     };
-
-    // console.log(uri);
-    // console.log(`width: ${width}`);
 
     if (!options.uri) {
       throw new Error("u (uri) parameter required");
@@ -24,7 +50,8 @@ function parseParams(request) {
       throw new Error("w parameter too small");
     }
 
-    // TODO enforce parameter order
+    // Ensure querystring format is consistent to maximize caching
+    validateCanonicalQuerystring(request, options);
 
     return options;
 }
