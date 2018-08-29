f18d7218cb7351fba8f6cb5d26f7ff879be41dc2
Laban Eilers <laban@eilers.com>
2018-08-29 09:24:40 -0400

    Remove code for debugging newrelic promise bug

--- server.js
+++ server.js
@@ -105,51 +105,6 @@ app.get('/frame', async (req, res, next) => {
   }
 });
 
-const util = require('util');
-let asyncTimeout = util.promisify(setTimeout);
-
-// Renders a page which contains an image with the same params
-// Useful for testing browser-sniffing and URL generation
-app.get('/timeoutdirect', async (req, res, next) => {
-  let timeout = parseInt(req.query.t) || 10000;
-  if (timeout > 0) {
-    await asyncTimeout(parseInt(req.query.t) || 10000);
-  }
-  res
-    .status(500)
-    .set({
-        'cache-control': 'no-cache',
-        'content-type': 'text/html'
-    })
-    .send('<html><head><title>Error</title></head><body><pre>Timeout testing</pre></body></html>');
-});
-
-app.get('/timeout', async (req, res, next) => {
-  let timeout = parseInt(req.query.t) || 10000;
-  if (timeout > 0) {
-    await asyncTimeout(parseInt(req.query.t) || 10000);
-  }
-  try {
-    throw new Error('Testing timeout');
-  } catch (ex) {
-    next(ex);
-  }
-});
-
-app.get('/success', async (req, res, next) => {
-  let timeout = parseInt(req.query.t) || 10000;
-  if (timeout > 0) {
-    await asyncTimeout(parseInt(req.query.t) || 10000);
-  }
-  res
-    .status(200)
-    .set({
-        'cache-control': 'no-cache',
-        'content-type': 'text/html'
-    })
-    .send('<html><head><title>Success</title></head><body><pre>Success</pre></body></html>');
-});
-
 // Error handling goes last
 app.use(errorHandling.middleware);
 app.listen(PORT, HOST);
